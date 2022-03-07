a95d4c33e50c9562055cd125116730277e0620d7
Remi Chuet <remschuet@gmail.5C5Ccom>
2022-03-06 19:33:46 -0500

    02

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-def find_participant(name: str, all:[]):
+from enum import Enum
+
+
+def find_participant(name: str, all: []):
     for item in all:
-        if item.nom.lower() == name.lower():
+        if item.name.lower() == name.lower():
             return item
     return None
+
+
+class weapon(Enum):
+    punch = 2
+    sword = 4
+    spear = 6
 
 
 class Human:
@@ -10,34 +19,45 @@ class Human:
     Classe qui définit un humain
     """
 
-    def __init__(self, class_nom: str, class_age: int, class_pv=15):
-        self.nom = class_nom
+    def __init__(self, class_name: str, class_age: int, class_pv: int, class_weapon=weapon.punch):
+        self.name = class_name
         self.age = class_age
         self.pv = class_pv
         self.alive = True
+        self.weapon = class_weapon
 
     def attack(self, opponent):
-        opponent.hurt()
+        opponent.hurt(self.weapon.value)
 
-    def hurt(self):
-        self.pv -= 5
+    def hurt(self, damage: int):
+        self.pv -= damage
         if self.pv <= 0:
             self.alive = False
 
+
 participants = []
-participants.append(Human("jason", 26, 15))
-participants.append(Human("bob", 22, 15))
+participants.append(Human("jason", 26, 15, weapon.sword))
+participants.append(Human("bob", 22, 10, weapon.punch))
 
 main_game = True
 while main_game:
-    name = input("Qui attaque?")
-    player1 = find_participant(name, participants)
+    choice = input("Voulez-vous attaquer? ")
+    player = find_participant("jason", participants)
+    enemy = find_participant("bob", participants)
 
-    if player1:
-        name = input("Qui est attaqué?")
-        player2 = find_participant(name, participants)
-        if player2:
-            player1.attack(player2)
+    if choice == "oui":
+        player.attack(enemy)
+        enemy.attack(player)
 
-    for participant in participants:
-        print(f"{participant.nom} a {participant.pv} points de vie")
+        for participant in participants:
+            print(f"{participant.name} a {participant.pv} points de vie")
+
+            # name = input("Qui attaque?")
+            # player1 = find_participant(name, participants)
+            #
+            # if player1:
+            #     name = input("Qui est attaqué?")
+            #     player2 = find_participant(name, participants)
+            #     if player2:
+            #         player1.attack(player2)
+            #
